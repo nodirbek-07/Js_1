69ab45d85ab7709a51869fa7f0178e8a5d6c1516
Nodirshox <nodirbekma07@gmail.com>
2022-08-14 13:43:28 +0500

    finished site

---
+++ main.js
@@ -0,0 +1,16 @@
+var ism = prompt('Ismingzini kiriting');
+var yosh = prompt('yoshingizni kiriting');
+
+var savol  = prompt('Misolni yeching 6 + 3= ?');
+var savol2  = prompt('Misolni yeching 10 - 3= ?');
+var savol3  = prompt('Misolni yeching 15 * 2= ?');
+var savol4  = prompt('Misolni yeching 30 / 3= ?');
+
+alert('Natijalar console da');
+
+console.log('Mening ismim ' + ism);
+console.log('Men ' + yosh + 'yoshdaman');
+console.log('Misol-1 : 6 + 3 = 9 ' + 'sizning javob 9');  
+console.log('Misol-2 : 10 - 3 = 7 ' + 'sizning javob 7');  
+console.log('Misol-1 : 15 * 2 = 30 ' + 'sizning javob 30');  
+console.log('Misol-1 : 30 / 3 = 10 ' + 'sizning javob 10');  
